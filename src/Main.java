846f3f8934f49dab2d4d668674259b349d354548
Bandita Chalise <chalisba@mail.uc.edu>
2024-09-23 09:49:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double price = 9.99;
+        double salesTax = 0.05 * price;
+        System.out.println("The price of the purchase is "+ price + " and the sales tax is "+ salesTax);
+
+    }
+}
